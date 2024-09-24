85a2568086245a1dcaae68bd3ae93707bdf566fc
Luhan Lacerda <luhanlacerda@gmail.com>
2024-09-24 20:02:56 -0300

    chore: expondo sg privado

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,22 +1,19 @@
 output "public_subnet_sg_id" {
-  value = aws_security_group.private_subnet_sg.id
+  value = aws_security_group.public_subnet_sg.id
 }
 
-output "security_group" {
-  description = "ID da security Group criada na AWS"
-  value       = aws_security_group.public_subnet_sg.id
+output "private_subnet_sg_id" {
+  value = aws_security_group.private_subnet_sg.id
 }
 
 output "rds_endpoint" {
   value = aws_db_instance.rds-lanchonete.endpoint
-  # sensitive = true
 }
 
 output "rds_aws_instance_identifier" {
-  value     = aws_db_instance.rds-lanchonete.identifier
-  sensitive = true
+  value = aws_db_instance.rds-lanchonete.identifier
 }
 
-output "aws_security_group" {
+output "aws_security_group_rds" {
   value = aws_security_group.sg-rds.id
 }
